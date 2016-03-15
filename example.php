2e9dd775b87d3b637fc3379851d520844da3855e
BrainstemStudio <mail@brainstemstudio.com>
2016-03-15 22:49:01 +0200

    Update example.php

--- example.php
+++ example.php
@@ -3,22 +3,22 @@
 	include('sparkpost-api.php');
 	
 	
-	$mail    = new sparkPostApi('https://api.sparkpost.com/api/v1/transmissions','dcc5c88ebac748da3cad37cec0e45c49ca6ec8b2');
+	$mail = new sparkPostApi('https://api.sparkpost.com/api/v1/transmissions','<YOUR API KEY>');
 
-	$mail   -> from(array('email' => 'validated@brainstemstudio.com','name' => 'BrainstemStudio'));
-	$mail   -> subject('My First Sparkpost Mail');
-	$mail   -> html('
+	$mail-> from(array('email' => 'validated@yourdomain.com','name' => 'Sender Name'));
+	$mail-> subject('My First Sparkpost Mail');
+	$mail-> html('
 		<h1>Mandrill is over</h1>
 		<p>Mandrill is now a paid service, let\'s move to sparkpost!</p>
 	');
 	
-	$mail   -> setTo(array('mail@brainstemstudio.com','mail.brainstemstudio@gmail.com'));
-	$mail->setReplyTo('mail@brainstemstudio.com');
+	$mail-> setTo(array('person1@yourdomain.com','person2@yourdomain.com'));
+	$mail->setReplyTo('youremail@yourdomain.com');
 	
-
+	/* CC emails as array same as "seTo"
 	//$mail->setCc(array('mail@brainstemstudio.com','mail.brainstemstudio@gmail.com'));
 
-
+	/* BCC emails as array same as "seTo" */
 	//$mail->setBcc(array('mail@brainstemstudio.com','mail.brainstemstudio@gmail.com'));
 	
 	$mail->send();
